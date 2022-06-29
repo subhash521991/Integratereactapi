269f5ccfd64e2d32ffa82713321fa87cd12ecbf5
Subhash Mourya <subhash@euronics.com>
2022-06-29 21:46:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+
+const [data, setData] = useState([])
+
+  useEffect(()=>{
+
+    fetch("https://jsonplaceholder.typicode.com/users").then((result)=>{
+
+      result.json().then((resp)=>{
+      
+      //console.warn("result", resp)
+      setData(resp)
+      
+      })
+      
+        })
+
+  },[])
+
+ //console.warn(data)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Get API Method</h1>
+<table border="1">
+<tbody>
+<tr>
+<td>id</td>
+<td>name</td>
+<td>username</td>
+<td>email</td>
+<td>phone</td>
+<td>website</td>
+</tr>
+{
+
+data.map((item)=>
+
+
+<tr  key={item.id}>
+<td>{item.id}</td>
+<td>{item.name}</td>
+<td>{item.username}</td>
+<td>{item.email}</td>
+<td>{item.phone}</td>
+<td>{item.website}</td>
+</tr>
+
+)
+
+
+}
+
+
+</tbody>
+</table>
+
     </div>
   );
 }
